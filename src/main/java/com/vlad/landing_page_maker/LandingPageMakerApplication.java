2dd1ccf7a2e422fa8f7dc3903e22832c5eb45a18
Vlad-Alexandru Nita <vlad.alexandru194@gmail.com>
2026-03-07 14:58:10 +0200

    init and added dotenv

---
+++ src/main/java/com/vlad/landing_page_maker/LandingPageMakerApplication.java
@@ -0,0 +1,18 @@
+package com.vlad.landing_page_maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandingPageMakerApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		System.setProperty("OPENAI_API_KEY", dotenv.get("OPENAI_API_KEY"));
+		SpringApplication.run(LandingPageMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vlad/landing_page_maker/LandingPageMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlad.landing_page_maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandingPageMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
